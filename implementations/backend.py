65cd94ba4624d993635d914ac28da3358968dbc4
Buco7854 <github@grimbert.net>
2024-11-13 21:02:29 +0100

    chore: typing

--- implementations/backend.py
+++ implementations/backend.py
@@ -5,13 +5,34 @@ import psycopg
 from psycopg_pool import ConnectionPool
 from psycopg.types.json import Jsonb
 
+from datetime import datetime, timedelta
+from typing import Dict, Any, List
+from interfaces import AbstractBackend
+import psycopg
+from psycopg_pool import ConnectionPool
+from psycopg.types.json import Jsonb
+
 class PostgreSQLBackend(AbstractBackend):
+    """
+    PostgreSQLBackend is a backend class for interacting with a PostgreSQL database
+    using a connection pool. It provides methods for handling transactions, fetching,
+    updating, inserting, and deleting data related to pages in the database.
+
+    Attributes:
+        _queued_counter (int): Counter to track the number of queued operations.
+        _pool (ConnectionPool): Connection pool used to manage database connections.
+        _pages_table (str): Name of the table storing page data.
+        _transaction_conn (Optional[psycopg.Connection]): The current active connection during a transaction.
+        _transaction_cur (Optional[psycopg.Cursor]): The current active cursor during a transaction.
+        _in_transaction (bool): Flag indicating whether a transaction is in progress.
+    """
+
     def __init__(self, pool: ConnectionPool):
         """
         Initializes the PostgreSQLBackend with necessary configurations.
 
         Args:
-            pool: A psycopg2 connection pool.
+            pool (ConnectionPool): A psycopg2 connection pool to manage database connections.
         """
         self._queued_counter = 0
         self._pool = pool
@@ -20,21 +41,30 @@ class PostgreSQLBackend(AbstractBackend):
         self._transaction_cur = None  # Holds the cursor during a transaction
         self._in_transaction = False  # Flag to track transaction state
 
-    def begin_transaction(self):
+    def begin_transaction(self) -> None:
         """
         Begins a transaction by setting up a dedicated connection and cursor
         that will be used for all subsequent database operations.
+
+        Raises:
+            RuntimeError: If a transaction is already in progress.
         """
         if self._in_transaction:
             raise RuntimeError("A transaction is already in progress.")
 
-        self._transaction_conn: psycopg.Connection = self._pool.getconn()
+        self._transaction_conn = self._pool.getconn()
         self._transaction_cur = self._transaction_conn.cursor()  # Create cursor here
         self._in_transaction = True
 
-    def end_transaction(self, commit: bool = True):
+    def end_transaction(self, commit: bool = True) -> None:
         """
         Ends the transaction by either committing or rolling back all changes.
+
+        Args:
+            commit (bool): Flag to indicate whether to commit the transaction. Defaults to True.
+
+        Raises:
+            RuntimeError: If no active transaction is in progress.
         """
         if not self._in_transaction:
             raise RuntimeError("No active transaction to end.")
@@ -52,24 +82,38 @@ class PostgreSQLBackend(AbstractBackend):
             self._transaction_cur = None  # Reset the cursor
             self._in_transaction = False
 
-    def in_transaction(self):
+    def in_transaction(self) -> bool:
+        """
+        Checks if a transaction is currently in progress.
+
+        Returns:
+            bool: True if a transaction is active, False otherwise.
+        """
         return self._in_transaction
 
-    def _get_connection(self):
+    def _get_connection(self) -> psycopg.Connection:
         """
         Returns the current connection to be used for operations.
         If a transaction is active, it returns the transaction connection.
-        """
 
+        Returns:
+            psycopg.Connection: The current database connection.
+        """
         if self._in_transaction:
             return self._transaction_conn
         else:
             return self._pool.getconn()
 
-    def _get_cursor(self, connection):
+    def _get_cursor(self, connection) -> psycopg.Cursor | psycopg.ServerCursor:
         """
         Returns the current cursor to be used for operations.
         If a transaction is active, it returns the transaction cursor.
+
+        Args:
+            connection (psycopg.Connection): The database connection to use for the cursor.
+
+        Returns:
+            psycopg.Cursor: The current database cursor.
         """
         if self._in_transaction:
             return self._transaction_cur
@@ -77,21 +121,24 @@ class PostgreSQLBackend(AbstractBackend):
             return connection.cursor()
 
     def get_urls(self, batch_size: int) -> List[str]:
-        one_day_ago = datetime.now() - timedelta(days=1)
-        args = []
+        """
+        Retrieves a list of URLs from the database that have not been crawled or
+        have been crawled more than a day ago.
 
-        query = f"SELECT url FROM {self._pages_table} WHERE last_crawled_at IS NULL"
+        Args:
+            batch_size (int): The number of URLs to retrieve.
+
+        Returns:
+            List[str]: A list of URLs fetched from the database.
+        """
+        query = "SELECT url FROM {self._pages_table} WHERE last_crawled_at IS NULL"
         if self._queued_counter % 5 == 0:
-            query += f" OR last_crawled_at < %s"
-            args.append(one_day_ago)
+            query += f" OR last_crawled_at < NOW() - 'INTERVAL 1 DAY'"
 
         query += " ORDER BY id LIMIT %s;"
-        args.append(batch_size)
-
-        # Get connection and cursor manually
         connection = self._get_connection()
         cursor = self._get_cursor(connection)
-        cursor.execute(query, tuple(args))
+        cursor.execute(query, (batch_size,))
         rows = cursor.fetchall()
 
         # If not in a transaction, commit and release the connection
@@ -104,12 +151,11 @@ class PostgreSQLBackend(AbstractBackend):
 
     def set_urls_as_queued(self, urls: List[str]) -> None:
         """
-        Mark url(s) as queued in the backend
+        Marks the specified URLs as queued in the database.
 
         Args:
-            urls (tuple): The page(s) to mark as queued.
+            urls (List[str]): A list of URLs to mark as queued.
         """
-        # Get connection and cursor manually
         connection = self._get_connection()
         cursor = self._get_cursor(connection)
         cursor.execute("UPDATE pages set queued='true' WHERE url = ANY(%s)", (urls,))
@@ -122,21 +168,30 @@ class PostgreSQLBackend(AbstractBackend):
         self._queued_counter += 1
 
     def release_urls(self, urls: List[str]) -> None:
+        """
+        Releases the specified URLs from the queued state in the database.
+
+        Args:
+            urls (List[str]): A list of URLs to release.
+        """
         query = f"UPDATE {self._pages_table} SET queued = FALSE WHERE url = ANY(%s)"
-        # Get connection and cursor manually
         connection = self._get_connection()
         cursor = self._get_cursor(connection)
         cursor.execute(query, (urls,))
 
-        # If not in a transaction, commit and release the connection
         if not self._in_transaction:
             connection.commit()
-            cursor.close()  # Close the cursor first
-            self._pool.putconn(connection)  # Release the connection back to pool
+            cursor.close()
+            self._pool.putconn(connection)
 
     def insert_pages(self, pages_data: List[Dict]) -> None:
+        """
+        Inserts a list of pages into the database using a batch insert stored procedure.
+
+        Args:
+            pages_data (List[Dict]): A list of dictionaries containing page data.
+        """
         query = "CALL batch_insert_pages(%s)"
-        # Get connection and cursor manually
         connection = self._get_connection()
         cursor = self._get_cursor(connection)
         cursor.execute(query, (Jsonb(pages_data),))
@@ -148,30 +203,38 @@ class PostgreSQLBackend(AbstractBackend):
             self._pool.putconn(connection)  # Release the connection back to pool
 
     def delete_pages(self, page_urls: List[str]) -> None:
+        """
+        Deletes the specified pages from the database.
+
+        Args:
+            page_urls (List[str]): A list of URLs of the pages to delete.
+        """
         query = f"DELETE FROM {self._pages_table} WHERE url = ANY(%s)"
-        # Get connection and cursor manually
         connection = self._get_connection()
         cursor = self._get_cursor(connection)
         cursor.execute(query, (page_urls,))
 
-        # If not in a transaction, commit and release the connection
         if not self._in_transaction:
             connection.commit()
-            cursor.close()  # Close the cursor first
-            self._pool.putconn(connection)  # Release the connection back to pool
+            cursor.close()
+            self._pool.putconn(connection)
 
     def increment_failed_tries(self, fail_mapping: Dict) -> None:
+        """
+        Increments the failed_tries counter for the specified URLs.
+
+        Args:
+            fail_mapping (Dict): A dictionary mapping URLs to the number of failed attempts.
+        """
         args = []
         for k, v in fail_mapping.items():
             args.append((v, k))
         query = f"UPDATE {self._pages_table} SET failed_tries = failed_tries + %s WHERE url = %s"
-        # Get connection and cursor manually
         connection = self._get_connection()
         cursor = self._get_cursor(connection)
         cursor.executemany(query, args)
 
-        # If not in a transaction, commit and release the connection
         if not self._in_transaction:
             connection.commit()
-            cursor.close()  # Close the cursor first
-            self._pool.putconn(connection)  # Release the connection back to pool
+            cursor.close()
+            self._pool.putconn(connection)
--- main.py
+++ main.py
@@ -4,35 +4,35 @@ import sys
 
 import logging
 import argparse
+from typing import Any
 
+import psycopg_pool
 import redis
-
-from psycopg_pool import ConnectionPool
 
 from implementations.crawler import Crawler
 from implementations.lock import RedisLock
 from implementations.repository import CrawlDataRepository
 from implementations import RedisCache, PostgreSQLBackend
 
-def get_env_variable(name, default=None):
+def get_env_variable(name, default=None) -> Any:
     """Helper to fetch environment variables with optional defaults."""
     return os.environ.get(name, default)
 
-def configure_logging(log_level):
+def configure_logging(log_level) -> None:
     """Configure logging for the application."""
     levels = logging.getLevelNamesMapping()
     logging.basicConfig(level=levels.get(log_level, logging.INFO), stream=sys.stdout)
 
-def create_connection_pool(config):
+def create_connection_pool(config) -> psycopg_pool.ConnectionPool:
     """Create and return a connection pool."""
-    return ConnectionPool(
+    return psycopg_pool.ConnectionPool(
         min_size=1,
         max_size=3,
         conninfo=f"dbname={config['db_name']} user={config['db_user']} password={config['db_password']} "
                  f"host={config['db_host']} port={config['db_port']}"
     )
 
-def create_redis_client(config):
+def create_redis_client(config) -> redis.Redis:
     """Create and return a Redis client."""
     return redis.Redis(
         host=config["redis_host"],
@@ -42,7 +42,7 @@ def create_redis_client(config):
         decode_responses=True
     )
 
-def configure_signal_handlers(crawler, redis_client, pool):
+def configure_signal_handlers(crawler, redis_client, pool) -> None:
     """Set up handlers for termination signals."""
     def handle_close(s, frame):
         logging.info("Stopping...")
@@ -57,64 +57,195 @@ def configure_signal_handlers(crawler, redis_client, pool):
     signal.signal(signal.SIGTERM, handle_close)
     signal.signal(signal.SIGINT, handle_close)
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
     """Parse and return command-line arguments."""
     parser = argparse.ArgumentParser(description='Crawler configuration')
 
     # Database arguments
-    parser.add_argument('--db_host', type=str, default=get_env_variable('DB_HOST'))
-    parser.add_argument('--db_name', type=str, default=get_env_variable('DB_NAME'))
-    parser.add_argument('--db_user', type=str, default=get_env_variable('DB_USER'))
-    parser.add_argument('--db_password', type=str, default=get_env_variable('DB_PASSWORD'))
-    parser.add_argument('--db_port', type=int, default=get_env_variable('DB_PORT'))
+    parser.add_argument(
+        '--db_host',
+        type=str,
+        default=get_env_variable('DB_HOST')
+    )
+
+    parser.add_argument(
+        '--db_name',
+        type=str,
+        default=get_env_variable('DB_NAME')
+    )
+
+    parser.add_argument(
+        '--db_user',
+        type=str,
+        default=get_env_variable('DB_USER')
+    )
+
+    parser.add_argument(
+        '--db_password',
+        type=str,
+        default=get_env_variable('DB_PASSWORD')
+    )
+
+    parser.add_argument(
+        '--db_port',
+        type=int,
+        default=get_env_variable('DB_PORT')
+    )
 
     # Redis arguments
-    parser.add_argument('--redis_host', type=str, default=get_env_variable('REDIS_HOST'))
-    parser.add_argument('--redis_port', type=int, default=get_env_variable('REDIS_PORT'))
-    parser.add_argument('--redis_db', type=int, default=get_env_variable('REDIS_DB', default=0))
+    parser.add_argument(
+        '--redis_host',
+        type=str,
+        default=get_env_variable('REDIS_HOST')
+    )
+
+    parser.add_argument(
+        '--redis_port',
+        type=int,
+        default=get_env_variable('REDIS_PORT')
+    )
+
+    parser.add_argument(
+        '--redis_db',
+        type=int,
+        default=get_env_variable('REDIS_DB', default=0)
+    )
 
     # Crawler configuration
-    parser.add_argument("--log_level", type=str, default=get_env_variable('LOG_LEVEL', default="INFO"))
-    parser.add_argument("--crawler_batch_size", type=int, default=get_env_variable('CRAWLER_BATCH_SIZE', default=300))
-    parser.add_argument("--crawler_queue_min_size", type=int, default=get_env_variable('CRAWLER_QUEUE_MIN_SIZE', default=50))
-    parser.add_argument("--crawler_deletion_candidates_max_size", type=int, default=get_env_variable('CRAWLER_DELETION_CANDIDATES_MAX_SIZE', default=100))
-    parser.add_argument("--crawler_failed_tries_max_size", type=int, default=get_env_variable('CRAWLER_FAILED_TRIES_MAX_SIZE', default=100))
-    parser.add_argument("--crawler_default_delay", type=int, default=get_env_variable('CRAWLER_DEFAULT_DELAY', default=3))
+    parser.add_argument(
+        "--log_level",
+        type=str,
+        default=get_env_variable('LOG_LEVEL', default="INFO")
+    )
+
+    parser.add_argument(
+        "--crawler_batch_size",
+        type=int,
+        default=get_env_variable('CRAWLER_BATCH_SIZE', default=300)
+    )
+
+    parser.add_argument(
+        "--crawler_queue_min_size",
+        type=int,
+        default=get_env_variable('CRAWLER_QUEUE_MIN_SIZE', default=50)
+    )
+
+    parser.add_argument(
+        "--crawler_deletion_candidates_max_size",
+        type=int,
+        default=get_env_variable('CRAWLER_DELETION_CANDIDATES_MAX_SIZE', default=100)
+    )
+
+    parser.add_argument(
+        "--crawler_failed_tries_max_size",
+        type=int,
+        default=get_env_variable('CRAWLER_FAILED_TRIES_MAX_SIZE', default=100)
+    )
+
+    parser.add_argument(
+        "--crawler_default_delay",
+        type=int,
+        default=get_env_variable('CRAWLER_DEFAULT_DELAY', default=3)
+    )
 
     # Browser configuration
     browser_headless = get_env_variable('BROWSER_HEADLESS', default="true").lower() in ['true', 't']
-    parser.add_argument("--browser_headless", type=bool, default=browser_headless)
-    parser.add_argument("--browser_port", type=int, default=get_env_variable('BROWSER_PORT', default=4444))
-    parser.add_argument("--browser_host", type=str, default=get_env_variable('BROWSER_HOST', default="localhost"))
-    parser.add_argument("--browser_user_agent", type=str, default=get_env_variable('BROWSER_USER_AGENT', default="GogolBot/SchoolProject"))
-    parser.add_argument("--browser_remote", type=bool, default=get_env_variable('BROWSER_REMOTE', default="false").lower() in ['true'])
 
-    parser.add_argument("--lock_name", type=str, default=get_env_variable('LOCK_NAME', default="crawler:db_lock"))
+    parser.add_argument(
+        "--browser_headless",
+        type=bool,
+        default=browser_headless
+    )
 
-    parser.add_argument("--cache_url_queue_key", type=str,
-                        default=get_env_variable('CACHE_URL_QUEUE_KEY', default="crawler:url_queue"))
-    parser.add_argument("--cache_page_list_key", type=str,
-                        default=get_env_variable('CACHE_PAGE_LIST_KEY', default="crawler:page_list"))
-    parser.add_argument("--cache_deletion_candidates_key", type=str,
-                        default=get_env_variable('CACHE_DELETION_CANDIDATES_KEY',
-                                                 default="crawler:deletion_candidates"))
-    parser.add_argument("--cache_failed_tries_list_key", type=str,
-                        default=get_env_variable('CACHE_FAILED_TRIES_LIST_KEY', default="crawler:failed_tries"))
-    parser.add_argument("--cache_robots_key_prefix", type=str,
-                        default=get_env_variable('CACHE_ROBOTS_KEY_PREFIX', default="crawler:robots"))
-    parser.add_argument("--cache_domain_next_crawl_key_prefix", type=str,
-                        default=get_env_variable('CACHE_DOMAIN_NEXT_CRAWL_KEY_PREFIX', default="crawler:next_crawl"))
-    parser.add_argument("--cache_domain_crawl_delay_key_prefix", type=str,
-                        default=get_env_variable('CACHE_DOMAIN_CRAWL_DELAY_KEY_PREFIX', default="crawler:crawl_delay"))
-    parser.add_argument("--cache_robots_cache_ttl", type=int,
-                        default=get_env_variable('CACHE_ROBOTS_CACHE_TTL', default=300))
-    parser.add_argument("--cache_max_in_memory_time", type=int,
-                        default=get_env_variable('CACHE_MAX_IN_MEMORY_TIME', default=600))
+    parser.add_argument(
+        "--browser_port",
+        type=int,
+        default=get_env_variable('BROWSER_PORT', default=4444)
+    )
+
+    parser.add_argument(
+        "--browser_host",
+        type=str,
+        default=get_env_variable('BROWSER_HOST', default="localhost")
+    )
+
+    parser.add_argument(
+        "--browser_user_agent",
+        type=str,
+        default=get_env_variable('BROWSER_USER_AGENT', default="GogolBot/SchoolProject")
+    )
+
+    parser.add_argument(
+        "--browser_remote",
+        type=bool,
+        default=get_env_variable('BROWSER_REMOTE', default="false").lower() in ['true']
+    )
+
+    parser.add_argument(
+        "--lock_name",
+        type=str,
+        default=get_env_variable('LOCK_NAME', default="crawler:db_lock")
+    )
+
+    # Cache arguments
+    parser.add_argument(
+        "--cache_url_queue_key",
+        type=str,
+        default=get_env_variable('CACHE_URL_QUEUE_KEY', default="crawler:urls_queue")
+    )
+
+    parser.add_argument(
+        "--cache_page_list_key",
+        type=str,
+        default=get_env_variable('CACHE_PAGE_LIST_KEY', default="crawler:pages_list")
+    )
+
+    parser.add_argument(
+        "--cache_deletion_candidates_key",
+        type=str,
+        default=get_env_variable('CACHE_DELETION_CANDIDATES_KEY', default="crawler:deletion_candidates_list")
+    )
+
+    parser.add_argument(
+        "--cache_failed_tries_list_key",
+        type=str,
+        default=get_env_variable('CACHE_FAILED_TRIES_LIST_KEY', default="crawler:failed_tries_list")
+    )
+
+    parser.add_argument(
+        "--cache_robots_key_prefix",
+        type=str,
+        default=get_env_variable('CACHE_ROBOTS_KEY_PREFIX', default="crawler:robots")
+    )
+
+    parser.add_argument(
+        "--cache_domain_next_crawl_key_prefix",
+        type=str,
+        default=get_env_variable('CACHE_DOMAIN_NEXT_CRAWL_KEY_PREFIX', default="crawler:next_crawl")
+    )
+
+    parser.add_argument(
+        "--cache_domain_crawl_delay_key_prefix",
+        type=str,
+        default=get_env_variable('CACHE_DOMAIN_CRAWL_DELAY_KEY_PREFIX', default="crawler:crawl_delay")
+    )
+
+    parser.add_argument(
+        "--cache_robots_cache_ttl",
+        type=int,
+        default=get_env_variable('CACHE_ROBOTS_CACHE_TTL', default=300)
+    )
+
+    parser.add_argument(
+        "--cache_max_in_memory_time",
+        type=int,
+        default=get_env_variable('CACHE_MAX_IN_MEMORY_TIME', default=600)
+    )
 
     # Parse and return the arguments
     return parser.parse_args()
 
-def main():
+def main() -> None:
     args = parse_args()
 
     config = vars(args)  # Convert Namespace to dict
@@ -122,13 +253,13 @@ def main():
     for k, v in config.items():
         if v is None:
             raise ValueError(f"key: {k} is required")
+
     # Configure logging
     configure_logging(config["log_level"])
 
     if config["browser_remote"]:
         os.environ["SELENIUM_REMOTE_URL"] = f"http://{config["browser_host"]}:{config["browser_port"]}/wd/hub/"
 
-    # Set up connections and resources
     pool = create_connection_pool(config)
     redis_client = create_redis_client(config)
 
@@ -171,7 +302,6 @@ def main():
     ]
     c = Crawler(cdr, cache, seed_list, config)
 
-    # Configure signal handlers (this is now only in the main thread)
     configure_signal_handlers(c, redis_client, pool)
 
     c.run()
